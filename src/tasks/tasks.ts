1a79f40fc431ace7080951cccef20d965952d948
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-11-26 10:53:44 +0500

    add V7 to I scales

--- src/tasks/tasks.ts
+++ src/tasks/tasks.ts
@@ -226,19 +226,34 @@ const INTERNAL_TASKS: Record<string, InternalTaskConfig> = {
 
   "Minor Pentatonic": {
     keyboardMapping: keyboard`
-      C4 Eb4 F4 G4 Bb4 C8 . . . . . Bb0
-      C3 Eb3 F3 G3 Bb3 C7 Eb7 F7 G7 Bb7 . .
-      C2 Eb2 F2 G2 Bb2 C6 Eb6 F6 G6 Bb6 .
-      C1 Eb1 F1 G1 Bb1 C5 Eb5 F5 G5 Bb5
+    C1 Eb1 F1 G1 Bb1 C5 Eb5 F5 G5 Bb5
+    C2 Eb2 F2 G2 Bb2 C6 Eb6 F6 G6 Bb6 
+    C3 Eb3 F3 G3 Bb3 C7 Eb7 F7 G7 Bb7 
+      C4 Eb4 F4 G4 Bb4 C8 . . . Bb0
+      
+      
+      
+    `,
+  },
+
+  "Blues Scale": {
+    keyboardMapping: keyboard`
+    C1 Eb1 F1 F#1 G1 Bb1 C5 Eb5 F5 F#5 G5 Bb5
+    C2 Eb2 F2 F#2 G2 Bb2 C6 Eb6 F6 F#6 G6 Bb6
+    C3 Eb3 F3 F#3 G3 Bb3 C7 Eb7 F7 F#7 G7 Bb7
+    C4 Eb4 F4 F#4 G4 Bb4 C8 . . Bb0
     `,
   },
 
   "Hirajoshi Scale": {
     keyboardMapping: keyboard`
-      C4 C#4 F4 F#4 Bb4 C8 . . . . . Bb0
-      C3 C#3 F3 F#3 Bb3 C7 C#7 F7 F#7 Bb7
-      C2 C#2 F2 F#2 Bb2 C6 C#6 F6 F#6 Bb6
-      C1 C#1 F1 F#1 Bb1 C5 C#5 F5 F#5 Bb5
+    C1 C#1 F1 F#1 Bb1 C5 C#5 F5 F#5 Bb5
+    C2 C#2 F2 F#2 Bb2 C6 C#6 F6 F#6 Bb6
+    C3 C#3 F3 F#3 Bb3 C7 C#7 F7 F#7 Bb7
+      C4 C#4 F4 F#4 Bb4 C8 . . . Bb0
+      
+      
+      
     `,
   },
 
@@ -248,6 +263,24 @@ const INTERNAL_TASKS: Record<string, InternalTaskConfig> = {
       G1 G2 G3 G4 G5 G6 G7 . . . . .
       C1 C2 C3 C4 C5 C6 C7 C8 . . .
       Eb1 Eb2 Eb3 Eb4 Eb5 Eb6 Eb7 . . .
+    `,
+  },
+
+  "V7 to I": {
+    keyboardMapping: keyboard`
+      G1 B1 D2 F2 G2 B2 D3 F3 G3 B3 D4 F4
+      G4 B4 D5 F5 G5 B5 D6 F6 G6 B6 D7 F7
+      C1 E1 G1 C2 E2 G2 C3 E3 G3 
+      C4 E4 G4 C5 E5 G5 C6 E6 G6 C7
+    `,
+  },
+
+  "V7 to i": {
+    keyboardMapping: keyboard`
+      G1 B1 D2 F2 G2 B2 D3 F3 G3 B3 D4 F4
+      G4 B4 D5 F5 G5 B5 D6 F6 G6 B6 D7 F7
+      C1 Eb1 G1 C2 Eb2 G2 C3 Eb3 G3 
+      C4 Eb4 G4 C5 Eb5 G5 C6 Eb6 G6 C7
     `,
   },
 };
