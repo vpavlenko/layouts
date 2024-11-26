6521a9b631018d7b50b01df64a94b61ce424348e
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-11-26 13:57:16 +0500

    maj7 chords

--- src/tasks/tasks.ts
+++ src/tasks/tasks.ts
@@ -310,6 +310,15 @@ const INTERNAL_TASKS: Record<string, InternalTaskConfig> = {
       
     `,
   },
+
+  "Maj7 Chords": {
+    keyboardMapping: keyboard`
+    B3 C4 C#4 D4 D#4 E4 F4 F#4 G4 G#4 A4 A#4
+    G3 G#3 A3 A#3 B3 C4 C#4 D4 D#4 E4 F4 F#4 
+    E3 F3 F#3 G3 G#3 A3 A#3 B3 C4 C#4 D4 E4 F4 F#4 
+    C3 C#3 D3 D#3 E3 F3 F#3 G3 G#3 A3 A#3 B3 C4
+    `,
+  },
 };
 
 // Transform internal format to exported format
