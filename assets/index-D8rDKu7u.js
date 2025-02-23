d23631b09b2dd1e834c720a5717b5ffb03d16f82
Vitaly Pavlenko <cxielamiko@gmail.com>
2025-02-23 18:37:49 +0400

    Updates

--- assets/index-D8rDKu7u.js
+++ assets/index-D8rDKu7u.js
@@ -421,10 +421,10 @@ In order to be iterable, non-array objects must have a [Symbol.iterator]() metho
     F3 C4 G3 D4 A3 E4 B3 F#4 C#4 G#4 D#4 A#4
     Bb2 F3 C3 G3 D3 A3 E3 B3 F#3 C#4 G#3 D#4 
     Db3 Ab3 Eb3 Bb3 F3 C4 G3 D4 A3 E4
-    `},"Chromatic Sequences":{keyboardMapping:Ge`
+    `},"Chromatic Verticals":{keyboardMapping:Ge`
     C2 C#2 D2 D#2 E2 F2 F#2 G2 G#2 A2 A#2 B2  
-    C4 C#4 D4 D#4 E4 F4 F#4 G4 G#4 A4 A#4 B4
     C3 C#3 D3 D#3 E3 F3 F#3 G3 G#3 A3 A#3 B3
+    C4 C#4 D4 D#4 E4 F4 F#4 G4 G#4 A4 A#4 B4
     C5 C#5 D5 D#5 E5 F5 F#5 G5 G#5 A5 A#5 B5
     `,colorMode:"flat-chromatic"},"Flat Chromatic Layout":{keyboardMapping:Ge`
     C2 C#2 D2 D#2 E2 F2 F#2 G2 G#2 A2 A#2 B2 
